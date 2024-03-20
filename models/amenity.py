364d685210aef2637eee17e49dba019c0fe795e2
efa07 <efatariku07@gmail.com>
2024-03-20 18:41:37 +0300

    major update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Tests for amenity """
+
+import models
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+# from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Class of Amenity
+    Attributes:
+        name: entry name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    # place_amenities = relationship('Place', secondary='place_amenity')
